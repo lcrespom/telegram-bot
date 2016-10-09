e86cdfa8851b78f75a496ac81f0a7d3f1e63905b
Luis Crespo <lcrespom@gmail.com>
2016-10-09 18:02:21 +0200

    Refactor and reduce tracing

--- listen.js
+++ listen.js
@@ -1,14 +1,11 @@
 const util = require('util');
 const TelegramBot = require('node-telegram-bot-api');
 
-let token;
-let count = 1;
-
+//-------------------- Heartbeat watchdog --------------------
 class HeartBeat {
 	constructor(timeout, handler) {
 		this.beatCount = 0;
 		setInterval(_ => {
-			console.log('... watchdog, beatCount:', this.beatCount);
 			if (this.beatCount == 0) handler();
 			this.beatCount = 0;
 		}, 1000 * timeout);
@@ -18,31 +15,22 @@ class HeartBeat {
 	}
 }
 
-parseCommandLine();
-hb = new HeartBeat(60, _ => process.exit());
-let bot = new TelegramBot(token);
-getUpdates(bot);
 
-function getUpdates(bot, nextUpdate) {
-	bot.getUpdates(10, 100, nextUpdate)
-	.then(updates => {
-		console.log('==> Updates:', util.inspect(updates, { colors: true, depth: 5 }));
-		if (updates.length > 0)
-			nextUpdate = updates.pop().update_id + 1;
-		return nextUpdate;
-	})
-	.catch(error => {
-		console.error('==> error:', e);
-		return -1;
-	})
-	.then(nextUpdate => {
-		hb.beat();
-		console.log('Looping with update_id =', nextUpdate, 'count:', count++);
-		setTimeout(_ => {
-			if (nextUpdate < 0) nextUpdate = undefined;
-			getUpdates(bot, nextUpdate);
-		}, 1000);
+//-------------------- Main --------------------
+
+let token;
+let count = 1;
+
+main();
+
+function main() {
+	parseCommandLine();
+	hb = new HeartBeat(60, _ => {
+		console.error('Watchdog timed out. Exiting.');
+		process.exit();
 	});
+	let bot = new TelegramBot(token);
+	getUpdates(bot);
 }
 
 function parseCommandLine() {
@@ -54,25 +42,51 @@ function parseCommandLine() {
 }
 
 
-//---------------------------------------------------------------
-// No longer used, but kept for refactoring into message handling
-//---------------------------------------------------------------
+//-------------------- Update listening & processing --------------------
 
-function onTextForever() {
-	let bot = new TelegramBot(token, { polling: true });
-	try {
-		listenMessages(bot);
-	}
-	catch (e) {
-		console.log('---------- Error ----------');
-		console.error(e);
-	}
+function getUpdates(bot, nextUpdate) {
+	bot.getUpdates(10, 100, nextUpdate)
+	.then(updates => {
+		if (updates.length > 0) {
+			for (let update of updates)
+				processUpdate(update);
+			nextUpdate = updates.pop().update_id + 1;
+		}
+		return nextUpdate;
+	})
+	.catch(error => {
+		console.error('==> error:', e);
+		return -1;
+	})
+	.then(nextUpdate => {
+		hb.beat();
+		setTimeout(_ => {
+			if (nextUpdate < 0) nextUpdate = undefined;
+			getUpdates(bot, nextUpdate);
+		}, 1000);
+	});
 }
 
-function listenMessages(bot) {
-	bot.onText(/.*/, msg => {
-		console.log('Got text:', msg);
-		var chatId = msg.chat.id;
-		bot.sendMessage(chatId, 'OK');
-	});
+function processUpdate(update) {
+	/* Update example:
+	{ update_id: number,
+    message:
+     { message_id: number,
+       from:
+        { id: number (same as chat id),
+          first_name: 'Name',
+          last_name: 'Surname',
+          username: 'username' },
+       chat:
+        { id: number,
+          first_name: 'Name',
+          last_name: 'Surname',
+          username: 'username',
+          type: 'private' },
+       date: 1476012617,
+       text: 'fish' } }
+	   */
+	  let chatId = update.message.chat.id;
+	  let msg = update.message.text;
+	  console.log('processUpdate - chatId:', chatId, '- msg:', msg);
 }
